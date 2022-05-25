a5fdc145ed0d11c1e777afddcfe5704ee1f2a88f
Nicole Ogawa <nogawa11@gmail.com>
2022-05-25 13:22:44 +0900

    added cards

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,7 +1,25 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+
 const Board = () => {
   return (
     <div className="board">
-
+      <div className="card"><FontAwesomeIcon icon={faCoffee} /></div>
+      <div className="card"></div>
+      <div className="card"></div>
+      <div className="card"></div>
+      <div className="card"></div>
+      <div className="card"></div>
+      <div className="card"></div>
+      <div className="card"></div>
+      <div className="card"></div>
+      <div className="card"></div>
+      <div className="card"></div>
+      <div className="card"></div>
+      <div className="card"></div>
+      <div className="card"></div>
+      <div className="card"></div>
+      <div className="card"></div>
     </div>
   )
 }
