45d4e26cb11bb0216ac0d97205509fc1d470db7a
Nicole Ogawa <nogawa11@gmail.com>
2022-05-30 12:01:22 +0900

    added icons to cards

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,31 +1,27 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+import { faStar } from '@fortawesome/free-solid-svg-icons'
+import { faClover } from '@fortawesome/free-solid-svg-icons'
+import { faHeart } from '@fortawesome/free-solid-svg-icons'
+import { faDiamond } from '@fortawesome/free-solid-svg-icons'
+import { faCircle } from '@fortawesome/free-solid-svg-icons'
+import { faCat } from '@fortawesome/free-solid-svg-icons'
+import { faDog } from '@fortawesome/free-solid-svg-icons'
 import { useState } from 'react'
 import Card from './Card'
 
 const Board = () => {
 
-
-
+  const icons = [faCoffee, faCoffee, faStar, faStar, faClover, faClover, faHeart, faHeart, faDiamond, faDiamond, faCircle, faCircle, faCat, faCat, faDog, faDog]
+  const cardElements = icons.map((icon) => {
+    return (
+      <Card cardIcon={icon} />
+    )
+  })
 
   return (
     <div className="board">
-      <Card cardIcon={faCoffee} />
-      <div className="card"></div>
-      <div className="card"></div>
-      <div className="card"></div>
-      <div className="card"></div>
-      <div className="card"></div>
-      <div className="card"></div>
-      <div className="card"></div>
-      <div className="card"></div>
-      <div className="card"></div>
-      <div className="card"></div>
-      <div className="card"></div>
-      <div className="card"></div>
-      <div className="card"></div>
-      <div className="card"></div>
-      <div className="card"></div>
+      {cardElements}
     </div>
   )
 }
