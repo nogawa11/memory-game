044009b91a5ed682e9fea080d2f508579d40b838
Nicole Ogawa <nogawa11@gmail.com>
2022-06-03 01:36:01 +0900

    refactored css

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import './App.css';
 import { useState, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCoffee } from '@fortawesome/free-solid-svg-icons'
