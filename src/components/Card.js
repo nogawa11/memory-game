902213bc9ab04427a2026adf029992c86a8645db
Nicole Ogawa <nogawa11@gmail.com>
2022-06-03 01:26:47 +0900

    refactored card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,20 +1,30 @@
-import { useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const Card = ({cardIcon, setCardOne, cardOne, setCardTwo, cardTwo, flipCards}) => {
 
   const handleSelection = (event) => {
-    if (cardOne === undefined && event.currentTarget.className !== "card match") {
+    if (
+      cardOne === undefined &&
+      event.currentTarget.className !== "card match"
+    ) {
       event.currentTarget.className = "card active"
       setCardOne(event.currentTarget)
-    } else if (cardOne !== undefined && cardTwo === undefined && cardOne !== event.currentTarget && event.currentTarget.className !== "card match") {
+    } else if (
+      cardOne !== undefined &&
+      cardTwo === undefined && cardOne !== event.currentTarget &&
+      event.currentTarget.className !== "card match"
+    ) {
       event.currentTarget.className = "card active"
       setCardTwo(event.currentTarget)
     }
   }
 
   return (
-    <div className={flipCards ? "card" : "card"} onClick={handleSelection} name={cardIcon.iconName}>
+    <div
+      className={flipCards ? "card" : "card"}
+      onClick={handleSelection}
+      name={cardIcon.iconName}
+    >
       <FontAwesomeIcon icon={cardIcon} />
     </div>
   )
